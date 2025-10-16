badead1e47ec9a6a40a8c982275553c7cb42e75a
Thiago Marcel <thipinta@gmail.com>
2025-10-16 11:21:00 -0300

    Add script to calculate student average from grades

---
+++ 2 semestre/tema 3/modulo3/desafio7.py
@@ -0,0 +1,9 @@
+a = int(input('Digite a primeira nota do aluno: '))
+print('A primeira nota do aluno é: ', end='')
+print(a)
+b = int(input('Digite a segunda nota do aluno: '))
+print('A segunda nota do aluno é: ', end='')
+print(b)
+c=(a+b)/2
+print('A média do aluno é: ', end='')
+print(c)
