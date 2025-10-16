740e161e099f93ee21cd2803f1e5659dc1292da2
Thiago Marcel <thipinta@gmail.com>
2025-10-16 09:13:24 -0300

    Add example script for user input and output

---
+++ 2 semestre/tema 3/python básico/exemplo1.py
@@ -0,0 +1,9 @@
+nome='Guanabara'
+idade=25
+peso=75.8
+print('Você sabia que', nome, 'tem', idade, 'anos e pesa', peso, 'quilos.')
+
+nome=input('Digite o seu nome: ')
+idade=input('Digite sua idade: ')
+peso=input('Digite sua peso: ')
+print('Você se chama', nome, ', tem', idade, 'anos e pesa', peso, 'quilos.')
