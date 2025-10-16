c88815226490455864e2be811cddbc27baecde9e
Thiago Marcel <thipinta@gmail.com>
2025-10-16 10:42:37 -0300

    Add basic arithmetic operations in exemplo1.py

---
+++ 2 semestre/tema 3/modulo3/exemplo1.py
@@ -0,0 +1,18 @@
+a=5
+b=2
+print('a =',a)
+print('b =',b)
+# + adição
+print(a,'+',b,'=',a+b)
+# - subtração
+print(a,'-',b,'=',a-b)
+# * multiplicação
+print(a,'*',b,'=',a*b)
+# / divisão
+print(a,'/',b,'=',a/b)
+# ** potencia
+print(a,'**',b,'=',a**b)
+# // divisão inteira
+print(a,'//',b,'=',a//b)
+# % resto da divisão
+print(a,'%',b,'=',a%b)
