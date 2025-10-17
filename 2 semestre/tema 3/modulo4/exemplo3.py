de70f7e27a327adc83d80bf1aedba4059e38b124
Thiago Marcel <thipinta@gmail.com>
2025-10-17 10:13:15 -0300

    Create exemplo3.py

---
+++ 2 semestre/tema 3/modulo4/exemplo3.py
@@ -0,0 +1,7 @@
+frase = 'Curso em Vídeo Python'
+print('frase = ', frase)
+vetor = frase.split()
+print('vetor = frase.split()')
+print('vetor = ', vetor)
+print('vetor[2] = ', vetor[2])
+print('vetor[2][3] = ', vetor[2][3])
