d86da65535eeed267ced84bb1c479c9dd1e7379a
Thiago Marcel <thipinta@gmail.com>
2025-10-17 09:14:03 -0300

    Create exemplo1.py

---
+++ 2 semestre/tema 3/modulo4/exemplo1.py
@@ -0,0 +1,11 @@
+# Linha a ser corrigida
+frase = 'Curso em Vídeo Python'
+
+# Imprime a frase original
+print('Frase original:', frase)
+
+# Correção: Salve o resultado da substituição na variável
+frase = frase.replace('Python', 'Android')
+
+# Agora a variável 'frase' contém o novo valor
+print('Frase modificada:', frase)
