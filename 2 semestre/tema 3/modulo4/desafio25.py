c600bcd0f694efe39b25ecb91e433cc213c5febc
Thiago Marcel <thipinta@gmail.com>
2025-10-17 11:42:41 -0300

    Create desafio25.py

---
+++ 2 semestre/tema 3/modulo4/desafio25.py
@@ -0,0 +1,15 @@
+# nome de uma pessoa:= nome
+a = input('Digite o nome de uma pessoa: ')
+
+if a.strip():
+    nome = a.strip()
+    print('Você digitou', nome.title())
+    vetor = nome.title().split()
+    print('O conjunto das palavras do nome da pessoa é', vetor)
+
+    if 'Silva' in vetor:
+        print('Então o nome da pessoa tem "Silva".')
+    else:
+        print('Então o nome da pessoa não tem "Silva".')
+else:
+    print('Você não digitou um nome válido.')
